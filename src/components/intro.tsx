969d00455d8538c052674be7a796063f33766e66
Girish <121732632+girish1208dev@users.noreply.github.com>
2024-04-07 13:01:42 +0530

    Update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,7 +1,28 @@
-import logo from '/assets/toxmo_logo.svg'
+import { useState, useEffect } from 'react';
+import logo from '/assets/toxmo_logo.svg';
 import { HashLink as Link } from 'react-router-hash-link';
+import { FaBars } from 'react-icons/fa';
 
 function Intro() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <header>
             <div className="container">
@@ -10,10 +31,25 @@ function Intro() {
                         <div className="left flex-row">
                             <img src={logo} alt="Toxmo Logo" width={'150px'} height={'50px'} />
                         </div>
-                        <div className="right flex-row hamburger" style={{ gap: '2rem' }}>
-                            <Link to="#product" smooth>Product</Link>
-                            <Link to="#waitlist" smooth>Join Waitlist</Link>
-                            <Link to="#contact" smooth>Contact</Link>
+                        <div className="right flex-row" style={{ gap: '2rem' }}>
+                            {/* Show hamburger icon only on mobile screens */}
+                            {isMobile && <FaBars className="hamburger" onClick={toggleMenu} />}
+                            {/* Dropdown menu */}
+                            {!isMobile && (
+                                <div className="dropdown-menu">
+                                <Link to="#product" smooth style={{ marginRight: '25px' }}>Product</Link>
+                                <Link to="#waitlist" smooth style={{ marginRight: '25px' }}>Join Waitlist</Link>
+                                <Link to="#contact" smooth>Contact</Link>
+                            </div>
+                            
+                            )}
+                            {isMobile && isMenuOpen && (
+                                <div className="dropdown-menu">
+                                    <Link to="#product" smooth>Product</Link><br></br>
+                                    <Link to="#waitlist" smooth>Join Waitlist</Link><br></br>
+                                    <Link to="#contact" smooth>Contact</Link><br></br>
+                                </div>
+                            )}
                         </div>
                     </ul>
                 </nav>
@@ -32,7 +68,7 @@ function Intro() {
                 </div>
             </div>
         </header >
-    )
+    );
 }
 
-export default Intro
+export default Intro;
