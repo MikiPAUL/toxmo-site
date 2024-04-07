12aa3a01a96c402140ab8c2b26dab07ea8416694
Girish <121732632+girish1208dev@users.noreply.github.com>
2024-04-07 13:03:45 +0530

    Update waitlist.tsx

--- src/components/waitlist.tsx
+++ src/components/waitlist.tsx
@@ -2,11 +2,11 @@ function WaitList() {
     return (
         <section style={{ backgroundColor: '#ffd500', color: 'white' }} id="waitlist">
             <div className="container">
-                <h3 style={{ textAlign: "center", paddingBottom: '1.5rem', color: '#1b1b1b', fontWeight: 400, lineHeight: 1.6 }}>
+                <h3 style={{ textAlign: "center", padding: '2.9rem', color: '#1b1b1b', fontWeight: 400, lineHeight: 1.6 }}>
                     YOUR HUNGRY IS FULFILLED IN SPECIAL WAY
                 </h3>
                 <h2 style={{ fontSize: '3rem', fontWeight: "bold", textAlign: "center", paddingBottom: '2rem' }}>Join the Waitlist</h2>
-                <form action="" method="post" className="waitlist__form flex-col" style={{ rowGap: '1.5rem' }}>
+                <form action="" method="post" className="waitlist__form flex-col" style={{ rowGap: '1.5rem', paddingBottom: '35px' }}>
                     <input type="text" name="name" id="name" placeholder="Name" />
                     <input type="email" name="email" id="email" placeholder="Email" />
                     <input type="text" name="phone-no" id="phone-no" placeholder="Phone Number" />
